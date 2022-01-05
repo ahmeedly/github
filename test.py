1b599611695ce5167263c8d84b84c54f3c8a1a41
premotor2021 <https://premotor2021@github.com>
2022-01-05 15:19:59 +0200

    add test.py file

---
+++ test.py
@@ -0,0 +1,22 @@
+"""def f1():
+    print("Called f1")
+
+
+def f2(f):
+    f()
+
+print(f1)"""
+
+# def fn(n):
+#     if (n == 0 or n == 1):
+#         return n
+#     return fn(n-1) + fn(n-2)
+
+# fn(5)
+
+n = None
+
+if n != '' and n is not None:
+    print('yes')
+else:
+    print('no')
